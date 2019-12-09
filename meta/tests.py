92f3263a6bce30047739bf2b718ee0767fd6e2dd
Jonasz Karwacki <jonaszakr@gmail.com>
2019-12-08 16:40:31 -0800

    add status code tests

--- meta/tests.py
+++ meta/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+class MetaTestCase(TestCase):
+    def test_404(self):
+        res = self.client.get('/dfhjjksjhkdsakjhs')
+        self.assertEqual(res.status_code, 404)
+        self.assertIn(b'<meta charset="utf-8">\n  '
+                      b'<title>Page Not Found</title>\n  '
+                      b'<meta name="viewport" content="width=device-width, initial-scale=1">',
+                      res.content)
+
+    def test_home(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 404)
