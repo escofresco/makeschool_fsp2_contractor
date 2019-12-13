b5dc19d15a985a2033fc697de4a51e0470b02fa5
Jonasz Karwacki <jonaszakr@gmail.com>
2019-12-13 07:28:13 -0800

    show home and article detail

--- news/views.py
+++ news/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import AnonymousArticle
+
+
+class ArticleDetailView(DetailView):
+    """Shows a page dispaying a specific article."""
+    model = AnonymousArticle
+
+    def get(self, request, slug):
+        """Hand down that article."""
+        article = self.get_queryset().get(slug__iexact=slug)
+        return render(request, "pages/article.html", {"article": article})
+
+
+class HomeView(TemplateView):
+    template_name = "pages/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["articles"] = AnonymousArticle.objects.all()
+        return context
